5624b47f49fc2474ea0a725894725ab4245cdd36
Tisca Catalin <admin@tiscacatalin.com>
2025-11-08 18:20:14 +0200

    edu reward token

---
+++ contracts/src/EduRewardToken.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin-contracts-5.5.0/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin-contracts-5.5.0/token/ERC20/extensions/ERC20Burnable.sol";
+import {AccessControl} from "@openzeppelin-contracts-5.5.0/access/AccessControl.sol";
+
+/**
+ * @title EduRewardToken
+ * @notice ERC20 token used for distributing rewards within the EduChain protocol.
+ *         Uses role-based access control for minting to support authorized reward distributors.
+ */
+contract EduRewardToken is ERC20, ERC20Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    /**
+     * @param admin The address that will receive DEFAULT_ADMIN_ROLE and MINTER_ROLE.
+     * @param initialSupply The amount of tokens to mint to the admin at deployment (18 decimals).
+     */
+    constructor(address admin, uint256 initialSupply) ERC20("EduReward", "RWD") {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(MINTER_ROLE, admin);
+        if (initialSupply > 0) {
+            _mint(admin, initialSupply);
+        }
+    }
+
+    /**
+     * @notice Mint new tokens to a recipient.
+     * @dev Callable only by accounts with MINTER_ROLE.
+     * @param to Recipient address.
+     * @param amount Token amount to mint (18 decimals).
+     */
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+}
+
+
